4d5236946e8abcc8bf4e4974308758d05e8c809c
mb.netmb <mb.netmb@09533882-05bd-11de-bbfd-a7c1bc4c261e>
2010-11-01 19:15:12 +0000

    Timeset Script hinzugefuegt

---
+++ var/scripts/timeset.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+  my ( $sec, $min, $hour, $mday, $mon, $year, $wday ) = localtime(time);
+  if ( $wday == 0 ) {
+    $wday = 6;
+  }
+  else {
+    $wday = $wday - 1;
+  }
+  
+system( "/opt/hap/bin/helper/hap-sendcmd2.pl", "destination 255 time-set day $wday hour $hour minute $min second $sec");
+
