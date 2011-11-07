28b86d7151dd7dc85533952abc30017a6d900fad
hap.wolff@gmail.com <hap.wolff@gmail.com@09533882-05bd-11de-bbfd-a7c1bc4c261e>
2011-11-07 09:33:27 +0000

    Der letzte Status eines Devices bleibt immer in der Tabelle Status erhalten.

--- bin/helper/hap-dbcleanup.pl
+++ bin/helper/hap-dbcleanup.pl
@@ -16,11 +16,18 @@ use HAP::Init;
 
 my $logDays;
 my $statusDays;
-GetOptions( "logdays|ld=s" => \$logDays, "statusdays|sd=s" => \$statusDays ) or die;
+my $sel;
+my $ref1; 
+my $config;
+GetOptions( "config|c=i" => \$config , "logdays|ld=s" => \$logDays, "statusdays|sd=s" => \$statusDays ) or die;
 
 die "Missing logdays [--logdays|ld] or statusdays [--statusdays|sd] Parameter\n" if (!$logDays && !$statusDays);
 
 my $c = new HAP::Init( FILE => "$FindBin::Bin/../../etc/hap.yml" );
+
+if (!$config) {
+ $config = $c->{DefaultConfig};
+}
 
 if ($logDays) {
   my $lTime = time - $logDays*86400;
@@ -30,12 +37,22 @@ if ($logDays) {
       $year + 1900,
       $mon + 1, $mday, $hour, $min, $sec
   );
+  print " Date $lTime \n";
   my $sth = $c->{dbh}->prepare("Delete from log where Time < \"$lTime\"");
   $sth->execute();
 }
 if ($statusDays) {
   my $sTime = time - $statusDays*86400;
-  my $sth = $c->{dbh}->prepare("Delete from status where TS < $sTime");
-  $sth->execute();
+  
+  my $sth = $c->{dbh}->prepare("SELECT Module, Address FROM status WHERE config = $config GROUP BY Module, Address");
+  $sth->execute(); 
+ 
+  while (my $ref = $sth->fetchrow_hashref()) {
+    $sel = $c->{dbh}->prepare("SELECT ts FROM status WHERE Module = $ref->{Module} AND Address = $ref->{Address} ORDER BY ts DESC LIMIT 1");
+    $sel->execute();
+    $ref1 = $sel->fetchrow_hashref();
+    $sel = $c->{dbh}->prepare("DELETE FROM status WHERE Module = $ref->{Module} AND Address = $ref->{Address} AND ts < $sTime AND ts <> $ref1->{ts}");
+    $sel->execute();
+  } 
 }
 exit 0;
