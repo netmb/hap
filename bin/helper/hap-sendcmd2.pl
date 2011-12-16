548dc4b75b595ddaa14523fdcfd05727041e06d5
mb.netmb <mb.netmb@09533882-05bd-11de-bbfd-a7c1bc4c261e>
2011-12-16 07:08:11 +0000

    Die Helper-Scripte hap-sendcmd und hap-sendcmd2 senden jetzt 5 Retransmits, wenn eine Antwort ausbleibt. Das sollte bei instabilen Funk-Verbidungen helfen.

--- bin/helper/hap-sendcmd2.pl
+++ bin/helper/hap-sendcmd2.pl
@@ -8,12 +8,20 @@ hap-sendcmd2.pl -  The Home Automation Project Send-Command Script (small versio
 =cut
 
 use IO::Socket::INET;
+my $retransmits = 5;
 my $host = "localhost";
 my $port = "7891";
 my $sock = new IO::Socket::INET( PeerAddr => $host, PeerPort => $port, Proto => 'tcp' );
 $sock or die "No socket :$!";
 my $obj = <$sock>;
 print $sock $ARGV[0]."\n";
-print <$sock>."\n";
+my $res = <$sock>;
+my $i = 0;
+while ($res =~ /.*\[ERR\].*/ && $i < $retransmits ) {
+  print $sock $ARGV[0]."\n";
+  my $res = <$sock>;
+  $i++;
+}
+print $res."\n";
 close $sock;
 exit;
