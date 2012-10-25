d44421464ab0119a0980b0957777cb4e3d748625
hap.wolff@gmail.com <hap.wolff@gmail.com@09533882-05bd-11de-bbfd-a7c1bc4c261e>
2012-10-25 10:41:08 +0000

    Test Branch für Erweiterungen

--- bin/hap-configserver.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-use FindBin ();
-use lib "$FindBin::Bin/../lib";
-use lib "$FindBin::Bin/hap-configserver/lib";
-require "$FindBin::Bin/hap-configserver/script/hapconfig_server.pl";
--- var/scripts/timeset.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-my ( $sec, $min, $hour, $mday, $mon, $year, $wday ) = localtime(time);
-if ( $wday == 0 ) {
-  $wday = 6;
-}
-else {
- $wday = $wday - 1;
-}
-system( "/opt/hap/bin/helper/hap-sendcmd2.pl", "destination 255 time-set day $wday hour $hour minute $min second $sec");
